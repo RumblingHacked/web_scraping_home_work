aecad4428dc0af100db61977b465c6290ac8ff11
RumblingHacked <rumblinghacked@gmail.com>
2019-01-23 09:34:45 -0600

    Skeleton Code uploaded

---
+++ scraping_homework.py
@@ -0,0 +1,42 @@
+#Import libraries that you will be using example sys
+import sys
+
+#Initiate entries as a global variable of the type list
+
+def wagner_noel():
+    venue_url = ''
+    venue_url_root = ''
+    venue = ''
+    venue_address = ''
+    venue_city = ''
+    venue_default_price = -1
+    price = venue_default_price
+    try:
+        soup = ''
+        # parse soup for dates
+        for '____' in soup.'_______'('____', '______'):
+            date = ''
+            try:
+                event_url = ''
+                event_soup = ''
+                name = ''
+                description = ''
+                time = ''
+                picture = ''
+                price = ''
+                '_______'.append({
+                    '''
+                    Create a dictionary for the entries and append them  to the entries list
+                    '''
+                })
+            except:
+                print(sys.exc_info())
+    except:
+        print("ERROR: Failed to retrieve calendar: "+venue_url)
+        print(sys.exc_info())
+
+def write_file():
+    '''
+    Write your own function that takes the
+    :return:
+    '''
